aafac6fa987be66312929222cf069c5fd277fae9
IPGARDE\sgrousset <sylvain.grousset1@gmail.com>
2023-09-22 11:02:49 +0200

    Ajout d'attributs pour refléter le nom exacts sur la base de données

--- Models/SessionParticipant.cs
+++ Models/SessionParticipant.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AirsoftManager_server.Models;
 
 [PrimaryKey(nameof(SessionId), nameof(Participant_id))]
-
+[Table("session_participant")]
 public partial class SessionParticipant
 {
+    [Column("session_id")]
     public int SessionId { get; set; }
 
+    [Column("participant_id")]
     public int Participant_id { get; set; }
 }
