10bc0354f131f64227a974fe8712a91348be1f5c
IPGARDE\sgrousset <sylvain.grousset1@gmail.com>
2023-09-22 11:02:04 +0200

    Ajout inscription du participant à une session en gérant les différents cas

--- Controllers/ParticipantController.cs
+++ Controllers/ParticipantController.cs
@@ -20,5 +20,59 @@ namespace AirsoftManager_server.Controllers
         {
             return Ok(_context.Participants.ToList());
         }
+
+        [HttpPost]
+        public IActionResult InscriptionParticipant(InscriptionParticipantModel leParticipant)
+        {
+            //Si le participant s'insrit pour la première fois, alors on le crée dans la base.
+            Participant participant = _context.Participants.Where(t => t.Email == leParticipant.email).FirstOrDefault();
+            
+            if(participant == null)
+            {
+                participant = new Participant
+                {
+                    Prenom = leParticipant.prenom,
+                    Nom = leParticipant.nom,
+                    Email = leParticipant.email
+                };
+
+                _context.Participants.Add(participant);
+                _context.SaveChanges();
+            }
+
+            //Le participant ne peut pas s'inscrire plusieurs fois à la même session.
+            SessionParticipant isAlreadyRegistered = _context.SessionParticipants.Where(t => t.Participant_id == participant.ParticipantId && t.SessionId == leParticipant.sessionID).FirstOrDefault();
+
+            if(isAlreadyRegistered == null)
+            {
+                return Ok(AddParticipantToSession(leParticipant.sessionID, participant.ParticipantId));
+            }
+            else
+            {
+                return Json("Already registered");
+            }
+
+        }
+
+        private bool AddParticipantToSession(int sessionID, int ParticipantID)
+        {
+            try
+            {
+                SessionParticipant sessionParticipant = new SessionParticipant
+                {
+                    Participant_id = ParticipantID,
+                    SessionId = sessionID,
+                };
+
+                _context.SessionParticipants.Add(sessionParticipant);
+                _context.SaveChanges();
+                return true;
+            }catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public record InscriptionParticipantModel(int sessionID, string nom, string prenom, string email);
     }
 }
