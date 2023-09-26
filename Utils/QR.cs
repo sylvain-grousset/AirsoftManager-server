216466a0231b18fe3da24257709b378c730d19bf
IPGARDE\sgrousset <sylvain.grousset1@gmail.com>
2023-09-26 14:04:38 +0200

    Ajout package QRCoder pour générer des QRCodes

---
+++ Utils/QR.cs
@@ -0,0 +1,21 @@
+﻿using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net;
+using System.Net.Mail;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AirsoftManager_server.Utils
+{
+    public class QR
+    {
+        public static string GenerateQR()
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("www.google.fr", QRCodeGenerator.ECCLevel.Q);
+            Base64QRCode qrCode = new Base64QRCode(qrCodeData);
+            return "data:image/png;base64," + qrCode.GetGraphic(20);
+        }
+
+    }
+}
